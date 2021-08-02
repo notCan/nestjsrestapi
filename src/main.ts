eec55a67ae374cc0c544158acf09e10023306f6d
Can DEVE <devecann@gmail.com>
2021-08-02 21:34:56 +0300

    bi şeyler oldu ama ne oldu emin değilim

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe(
-    {
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //isteğin gönderilen bütün verileri değil DTO kısmında belirtilen verileri çekmesini sağlayan kod parçası
       whitelist: true,
-    }
-  ));
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
